2d0cf6458175b2cd1773682036fe7571d091d58b
Ankur Sundara <me@ankursundara.com>
2016-07-28 19:22:54 -0400

    Make solution better

--- crypto/blocky-200/blocky_sol.py
+++ crypto/blocky-200/blocky_sol.py
@@ -1,10 +1,10 @@
 from z3 import *
-import binascii, string, itertools
+import binascii, string
+
 g = lambda a, b, q: (a << b%q) & (2**q-1) | ((a & (2**q-1)) >> (q-(b%q)))
 h = lambda a, b, q: ((a & (2**q-1)) >> b%q) | (a << (q-(b%q)) & (2**q-1))
 
 bits = 40
-mask = 2**bits - 1
 allowed_chars = string.printable
 
 def int2ascii(x):
@@ -18,17 +18,19 @@ def int2ascii(x):
     x-=(x//256)*256
     txt+= chr(x)
     return txt
+
 def ascii2int(blah):
     return ord(blah[4])+256*ord(blah[3])+65536*ord(blah[2])+16777216*ord(blah[1])+4294967296*ord(blah[0])
 
 def rot(val, steps):
    return (val << (bits-steps)) | LShR(val, steps)
 
-def symencrypt(input):
-   eax = rot(input ^ 0x666c616721, (input ^ 0x666c616721) & 0xf)
-   eax = rot(eax + 0x4e4f504521, bits - (eax + 0x4e4f504521 & 0xf))
-   eax = eax * 0x6861686121
-   return eax & mask
+def symencrypt(x):
+   ret = rot(x ^ 0x666c616721, (x ^ 0x666c616721) & 0xf)
+   ret = rot(ret + 0x4e4f504521, bits - (ret + 0x4e4f504521 & 0xf))
+   ret *= 0x6861686121
+   ret &= 2**40-1
+   return ret
 
 def encrypt(x):
     ret = h(x ^ 0x666c616721, (x ^ 0x666c616721) & 0xf, 40)
@@ -38,7 +40,7 @@ def encrypt(x):
     return ret
 
 
-known = '6915c70109fa3398321127cfcd44342115d6d75feb56706087'
+known = '200b719a63f408188f9e51088df43f90b9b8d173'
 data = list(map(''.join, zip(*[iter(known)]*10)))
 print data
 vs = [int(x, 16) for x in data]
@@ -50,33 +52,27 @@ for x in allowed_chars:
     enc = encrypt(ascii2int('ctf('+x))
     poss_ivs.append(enc^vs[0])
 print poss_ivs
-#vs[0]^=0xde4dbe3f49
-#for i in range(1,len(vs)):
-#    vs[i]^=vs[i-1]
-#print "vs:",vs
-
 
 def undoBlockEncrypt(x):
     s = Solver()
     first = BitVec('bitvec', bits)
     s.add(symencrypt(first)==x)
+    sols = []
     while s.check() == sat:
         sol = s.model()
         vec = sol[first].as_long()
         s.add(first != vec)
         text = int2ascii(vec)
         print text
-        #allASCII = True
-        #for c in text:
-        #    if c not in allowed_chars:
-        #        allASCII= False
-        #if allASCII:
-        #    print "text:", text
 
-#for x in poss_ivs:
-#    print x
-#    undoBlockEncrypt(x^vs[1]^vs[0])
-vs[0]^=0xde4dbe3f49
+
+# figure out IV based on whether 2nd block decrypts to ASCII
+for x in poss_ivs:
+    print x
+    undoBlockEncrypt(x^vs[1]^vs[0])
+
+
+vs[0]^=954787053385 # the IV
 undoBlockEncrypt(vs[0])
 for i in range(1,len(vs)):
     vs[i]^=vs[i-1]
